8148ccd8afe4b9c3cc5f0d0702c84639584620d8
Gabe-Biele <gabriel.biele@gmail.com>
2016-11-12 20:19:43 -0500

    0.0.5 - Projectiles

---
+++ Assets/Scripts/Game/Triggers/ProjectileTrigger.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileTrigger : MonoBehaviour
+{
+    private bool inField = false;
+    private float secondsLeft;
+    private Transform playerTF;
+
+    public float SecondsPerProjectile;
+    public float ProjectileForce;
+    public GameObject ProjectileObject;
+    public GameObject ProjectileSource;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        playerTF = GameObject.Find("Player").transform;
+    }
+
+    void OnTriggerEnter(Collider objectCollider)
+    {
+        if(objectCollider == playerTF.GetComponentInChildren<MeshCollider>())
+        {
+            inField = true;
+        }
+    }
+
+    void OnTriggerExit(Collider objectCollider)
+    {
+        if(objectCollider == playerTF.GetComponentInChildren<MeshCollider>())
+        {
+            inField = false;
+        }
+    }
+
+
+    // Update is called once per frame
+    void FixedUpdate ()
+    {
+	    if(inField)
+        {
+            secondsLeft = secondsLeft - Time.deltaTime;
+            if(secondsLeft <= 0)
+            {
+                secondsLeft = SecondsPerProjectile;
+                GameObject aProjectile = (GameObject)Instantiate(ProjectileObject);
+                aProjectile.transform.position = ProjectileSource.transform.position;
+                aProjectile.GetComponent<Rigidbody>().AddForce(((Vector3)(playerTF.position - aProjectile.transform.position)).normalized * ProjectileForce);
+            }
+        }
+	}
+}
